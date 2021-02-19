97b6a8510ad146c9415b7291b7aef2e2f6286b62
PHOENiX <rlaphoenix@pm.me>
2021-02-19 01:02:32 +0000

    Create formatter.py

---
+++ formatter.py
@@ -0,0 +1,31 @@
+import re
+import textwrap
+from string import Formatter
+
+
+class CustomFormats(Formatter):
+    def format_field(self, value, format_spec: str):
+        if format_spec in ("true", "!false"):
+            # e.g. {var:true} will return 1 if var is a truthy value, {var:!false} is an identical alternative
+            return "1" if value else "0"
+        if format_spec in ("false", "!true"):
+            # e.g. {var:false} will return 1 if var is not a truthy value, {var:!true} is an identical alternative
+            return "0" if value else "1"
+        if re.match(r"^>>\d+:\d+$", format_spec):
+            # e.g. {var:>>2:68} will textwrap each line (at 68 chars), and each line will be indented by 2 spaces
+            indent, chars = [int(x) for x in format_spec[2:].split(":")]
+            if isinstance(value, list):
+                value = self.list_to_indented_strings(value, indent)
+                return value
+            return "\n".join(textwrap.wrap(value, chars, subsequent_indent=" " * indent))
+        if re.match(r"^\^>\d+:\d+$", format_spec):
+            # e.g. {var:^>70:68} will center each line (at 70 chars) and textwrap each line (at 68 chars)
+            center, wrap = [int(x) for x in format_spec[2:].split(":")]
+            return "\n".join([x.center(center) for x in textwrap.wrap(value, wrap)])
+        return super().format_field(value, format_spec)
+
+    def list_to_indented_strings(self, value: list, indent: int = 0):
+        """Recursively convert a list to an indented \n separated string."""
+        if isinstance(value[0], list):
+            return self.list_to_indented_strings(value[0], indent)
+        return f"\n{' ' * indent}".join(value)
