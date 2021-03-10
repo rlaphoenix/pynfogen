1145ac4d0037a07406ba570151f084d7b2e2e249
PHOENiX <rlaphoenix@pm.me>
2021-03-10 00:23:34 +0000

    helpers.scrape: Don't manually set Accept-Encoding

--- helpers.py
+++ helpers.py
@@ -8,7 +8,6 @@ def scrape(url):
             # pretend to be a normal firefox user, we can't leave anything to chance
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, br",
             "Accept-Language": "en-US,en;q=0.5",
             "DNT": "1",
             "UPGRADE-INSECURE-REQUESTS": "1"
