5ef82e9799761b6382f1843b4741105a753f5f6e
rlaphoenix <rlaphoenix@pm.me>
2021-08-27 01:02:40 +0100

    template: Type-hint returns, remove term bbcode

--- pynfogen/cli/template.py
+++ pynfogen/cli/template.py
@@ -8,17 +8,17 @@ from pynfogen.helpers import open_file
 
 
 @click.group()
-def template():
+def template() -> None:
     """Manages template files."""
 
 
 @template.command()
 @click.argument("name", type=str)
-@click.option("--bbcode", is_flag=True, default=False, help="Specify template as a BBCode Description template.")
-def edit(name: str, bbcode: bool):
+@click.option("-d", "--description", is_flag=True, default=False, help="Specify template as a Description template.")
+def edit(name: str, description: bool) -> None:
     """Edit a template file. If one does not exist, one will be created."""
     log = logging.getLogger("template")
-    location = Path(str(Files.description if bbcode else Files.template).format(name=name))
+    location = Path(str(Files.description if description else Files.template).format(name=name))
     if not location.exists():
         log.info(f"Creating new template named {name}")
         location.parent.mkdir(exist_ok=True, parents=True)
@@ -30,12 +30,12 @@ def edit(name: str, bbcode: bool):
 
 @template.command()
 @click.argument("name", type=str)
-@click.option("--bbcode", is_flag=True, default=False, help="Specify template as a BBCode Description template.")
+@click.option("-d", "--description", is_flag=True, default=False, help="Specify template as a Description template.")
 @click.confirmation_option(prompt="Are you sure you want to delete the template?")
-def delete(name: str, bbcode: bool):
+def delete(name: str, description: bool) -> None:
     """Delete a template file."""
     log = logging.getLogger("template")
-    location = Path(str(Files.description if bbcode else Files.template).format(name=name))
+    location = Path(str(Files.description if description else Files.template).format(name=name))
     if not location.exists():
         raise click.ClickException(f"Template {name} does not exist.")
     location.unlink()
@@ -43,20 +43,20 @@ def delete(name: str, bbcode: bool):
 
 
 @template.command(name="list")
-def list_():
+def list_() -> None:
     """List all available templates."""
     found = 0
     for nfo in Directories.templates.glob("*.nfo"):
-        print(nfo.stem, "-", "NFO", "Template")
+        print(nfo.stem, "-", "NFO Template")
         found += 1
     for txt in Directories.templates.glob("*.txt"):
-        print(txt.stem, "-", "BBCode", "Template")
+        print(txt.stem, "-", "Description Template")
         found += 1
     if not found:
         raise click.ClickException("No templates found.")
 
 
 @template.command()
-def explore():
+def explore() -> None:
     """Open the template directory in your File Explorer."""
     open_file(str(Directories.templates))
