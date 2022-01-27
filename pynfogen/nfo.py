ea18b5ddeb44960ce673cb200d897d0a017717f4
rlaphoenix <pragma.exe@gmail.com>
2022-01-27 21:53:04 +0000

    Simplify request session headers

--- pynfogen/nfo.py
+++ pynfogen/nfo.py
@@ -437,9 +437,6 @@ class NFO:
         session = requests.Session()
         session.headers.update({
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.5",
-            "DNT": "1",
-            "UPGRADE-INSECURE-REQUESTS": "1"
+            "Accept-Language": "en-US,en;q=0.5"
         })
         return session
